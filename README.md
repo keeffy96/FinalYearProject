5a7622fb34b2772b9c1f79b352089a1429ed274d
Keeffy96 <Keeffy96@gmail.com>
2017-10-24 09:01:09 +0100

    Updated profile page

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# FinalYearProject
+# Computational Thinking Engine
+
+The vision:
+A tool that allows teachers to access, customise and interact with the curriculum. A place that students can log on to take assessment and feedback surveys, as well as perhaps do some interactive problems and games. 
+
+--------------------------------------------------
+
+Getting started:
+1) download python (v.3.6.3)
+ - make sure python is added to your path
+2)open command prompt and run:
+ - pip install Flask
+ - pip install Flask-PyMongo
+ - pip install bcrypt
+
+To run the project:
+1) Open command prompt
+2) Go to directory with python script eg. cd Documents...
+3) run -> python filename.py
+4) Open browser and enter 127.0.0.1:5000 
+
+Or:
+1) Using the Sublime Text 3 text editor
+2) open python script and click ctrl-b
+3) Open browser and enter 127.0.0.1:5000 
+
+------------------------------------------------------------
+
+Using mLab - mLab is the largest cloud MongoDB service in the world.
+
+In mLab I have a users collection: 
+Student:
+user_type,
+school,
+name,
+surname,
+approved,
+user_id,
+password
+
+Instructor:
+user_type,
+school,
+title,
+name,
+surname,
+email,
+password
+
+What i have so far:
+Home page:
+Register for both instructor & student
+Sign in
+Profile
+
+Issues: Students cant sign in yet.
--- login_example.py
+++ login_example.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
+import random
 
 app = Flask(__name__)
 
@@ -13,24 +14,35 @@ mongo = PyMongo(app)
 def home():
     return render_template('home.html')
 
-@app.route('/index')
-def index():
+@app.route('/signIn')
+def signIn():
     if 'email' in session:
-        return 'You are logged in as ' + session['email']
+        email = session['email']
+        return redirect(url_for('profile'))
 
-    return render_template('index.html')
+    #elif 'user_id' in session:
+     #   user_id = session['user_id']
+      #  return redirect(url_for('profile'))
+
+    else:
+        return render_template('user_authentication/signIn.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-    login_user = users.find_one({'email' : request.form['email']})
+    login_instructor = users.find_one({'email' : request.form['email']})
 
-    if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password'] :
+    if login_instructor:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_instructor['password']) == login_instructor['password'] :
             session['email'] = request.form['email']
-            return redirect(url_for('index'))
+            return redirect(url_for('signIn'))
 
     return 'Invalid email/password combination'
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return render_template('home.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -40,26 +52,92 @@ def register():
 
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            user_type = 'instructor'
+            school = request.form['school']
+            title = request.form['title']
             name = request.form['name']
             surname = request.form['surname']
             email = request.form['email']
-            users.insert({'name' : name, 'surname': surname, 'email' : email, 'password' : hashpass})
+            users.insert({'user_type':user_type,'school': school, 'title': title, 'name' : name, 'surname': surname, 'email' : email, 'password' : hashpass})
             session['email'] = request.form['email']
-            return redirect(url_for('index'))
+            return redirect(url_for('signIn'))
         
         return 'That email already exists!'
 
-    return render_template('register.html')
+    return render_template('user_authentication/register.html')
 
 @app.route('/studentRegister', methods=['POST', 'GET'])
 def studentRegister():
     if request.method == 'POST':
-        babras1 = mongo.db.babras1
-        q1 = request.form['q1']
-        q2 = request.form['q2']
-        babras1.insert({'q1':q1, 'q2': q2})
+        users = mongo.db.users
+        user_type = 'student'
+        school = request.form['school']
+        name = request.form['name']
+        surname = request.form['surname']        
+        hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+        approved = 0
+        user_id = random.randint(1,1000) 
+        users.insert({'user_type': user_type, 'school': school, 'name': name, 'surname': surname, 'approved':approved, 'user_id':user_id, 'password' : hashpass})
+        #session['user_id'] = user_id
+        #return redirect(url_for('signIn'))
 
-    return render_template('studentRegister.html')
+    return render_template('user_authentication/studentRegister.html')
+
+@app.route('/babras', methods=['POST', 'GET'])
+def studentBabrasTest1():
+    if request.method == 'POST':
+        babras1 = mongo.db.babras1
+        answer1 = request.form['answer1']
+        answer2 = request.form['answer2']
+        answer3 = request.form['answer3']
+        answer4 = request.form['answer4']
+        answer5 = request.form['answer5']
+        answer6 = request.form['answer6']
+        babras1.insert({'answer1': answer1, 'answer2': answer2, 'answer3': answer3, 'answer4': answer4, 'answer5': answer5, 'answer6': answer6})
+
+    return render_template('studentBabrasTest1.html')
+
+@app.route('/profile')
+def profile():
+    if 'email' in session:
+        users = mongo.db.users
+        name = users.find_one({'email':session['email']})['name']
+        surname = users.find_one({'email':session['email']})['surname']
+
+        #loops db and gets all instances where approved is 0
+        student = [item for item in users.find({'approved': 0})]
+        instructorSchool = users.find_one({'email':session['email']})['school']
+
+        keys = ['name', 'surname', 'user_id']
+
+        student = [student[0].get(key) for key in keys]
+
+        return render_template('profile.html', name=name, surname=surname, student=student, instructorSchool=instructorSchool)
+
+    return redirect(url_for('signIn')) 
+
+@app.route('/editProfile', methods=['POST', 'GET'])
+def editProfile():
+    if request.method == 'POST':
+        users = mongo.db.users
+        name = request.form['name']
+        surname = request.form['surname']
+        users.update_one({'email':session['email']}, {'$set': {'name': name, 'surname': surname}})
+        return redirect(url_for('profile'))
+    
+    return render_template('editProfile.html')
+
+@app.route('/approveStudent', methods=['POST'])
+def approveStudent():
+    users = mongo.db.users
+    studentRadio = request.form['studentRadio']
+    #student = [item for item in users.find({'approved': 0})]
+    result = users.update_one({'user_id': 265}, {'$set': {'approved' : 0}})
+
+    return redirect(url_for('profile'))
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
